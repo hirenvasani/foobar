c9cdb9044b4459902c8dda74179b806a2e639a6b
Hiren Vasani <hirenvasani@users.noreply.github.com>
2017-06-12 21:12:24 +0530

    Create dont_get_volunteered.py

--- dont_get_volunteered.py
+++ dont_get_volunteered.py
@@ -47,3 +47,32 @@ Output:
 
 Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
 '''
+
+
+def cord(src):
+    return src / 8, src % 8
+
+def sht_pth(sx, sy, board):
+    arr = [(sx, sy)]
+    while arr:
+        x, y = arr.pop(0)
+        for i in [[1,2],[-1,2],[1,-2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]]:
+          nx, ny = x + i[0], y + i[1]
+          if 0 <= nx <= 7 and 0 <= ny <= 7:
+              if board[nx][ny] is None:
+                  board[nx][ny] = board[x][y] + 1
+                  arr.append((nx, ny)) 
+
+def answer(src, dest):
+    board = [[None for i in range(8)] for i in range(8)]
+    sx, sy = cord(src)
+    dx, dy = cord(dest)
+    board[sx][sy] = 0
+    sht_pth(sx, sy, board)
+    return board[dx][dy]
+
+#print answer(19, 36)
+# Answer 31
+
+print answer(0, 1)
+# Answer 3
