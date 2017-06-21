06c9350b4c5efea516b31e26653fe8a9a04fcc60
Hiren Vasani <hirenvasani@users.noreply.github.com>
2017-06-21 20:10:06 +0530

    Create prepare_the_bunnies_escape.py

---
+++ prepare_the_bunnies_escape.py
@@ -0,0 +1,72 @@
+'''
+Challenge 3.1
+Prepare the Bunnies' Escape
+===========================
+
+You're awfully close to destroying the LAMBCHOP doomsday device and freeing Commander Lambda's bunny prisoners, but once they're free of the prison blocks, the bunnies are going to need to escape Lambda's space station via the escape pods as quickly as possible. Unfortunately, the halls of the space station are a maze of corridors and dead ends that will be a deathtrap for the escaping bunnies. Fortunately, Commander Lambda has put you in charge of a remodeling project that will give you the opportunity to make things a little easier for the bunnies. Unfortunately (again), you can't just remove all obstacles between the bunnies and the escape pods - at most you can remove one wall per escape pod path, both to maintain structural integrity of the station and to avoid arousing Commander Lambda's suspicions. 
+
+You have maps of parts of the space station, each starting at a prison exit and ending at the door to an escape pod. The map is represented as a matrix of 0s and 1s, where 0s are passable space and 1s are impassable walls. The door out of the prison is at the top left (0,0) and the door into an escape pod is at the bottom right (w-1,h-1). 
+
+Write a function answer(map) that generates the length of the shortest path from the prison door to the escape pod, where you are allowed to remove one wall as part of your remodeling plans. The path length is the total number of nodes you pass through, counting both the entrance and exit nodes. The starting and ending positions are always passable (0). The map will always be solvable, though you may or may not need to remove a wall. The height and width of the map can be from 2 to 20. Moves can only be made in cardinal directions; no diagonal moves are allowed.
+
+Languages
+=========
+
+To provide a Python solution, edit solution.py
+To provide a Java solution, edit solution.java
+
+Test cases
+==========
+
+Inputs:
+    (int) maze = [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]
+Output:
+    (int) 7
+
+Inputs:
+    (int) maze = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
+Output:
+    (int) 11
+    
+'''
+
+def sht_pth(sx, sy, maze):
+    w = len(maze[0])
+    h = len(maze)
+    board = [[None for i in range(w)] for i in range(h)]
+    board[sx][sy] = 1
+
+    arr = [(sx, sy)]
+    while arr:
+        x, y = arr.pop(0)
+        for i in [[1,0],[-1,0],[0,-1],[0,1]]:
+          nx, ny = x + i[0], y + i[1]
+          if 0 <= nx < h and 0 <= ny < w:
+            if board[nx][ny] is None:
+                board[nx][ny] = board[x][y] + 1
+                if maze[nx][ny] == 1 :
+                  continue
+                arr.append((nx, ny)) 
+                  
+    return board
+
+def answer(maze):
+  w = len(maze[0])
+  h = len(maze)
+  tb = sht_pth(0, 0, maze)
+  bt = sht_pth(h-1, w-1, maze)
+  board = []
+
+  ans = 2 ** 32-1
+  for i in range(h):
+      for j in range(w):
+          if tb[i][j] and bt[i][j]:
+              ans = min(tb[i][j] + bt[i][j] - 1, ans)
+  return ans
+
+#maze = [[0, 0, 0, 0, 0, 0],[1, 1, 1, 1, 1, 0],[1, 1, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0],[0, 1, 1, 1, 1, 1],[0, 0, 0, 0, 0, 0]] #Answer 21
+#maze = [[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]] #Answer 7
+#maze = [[0,1,0,0,0],[0,0,0,1,0],[1,1,1,1,0]] #Answer 7
+#maze = [[0,1,1,1],[0,1,0,0],[1,0,1,0],[1,1,0,0]] #Answer 7
+maze = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]] #Answer 11
+print answer(maze)
